c9ad07971bd9f26890cb9dea677cbbe0062ae287
moonstar0331 <moonsung0331@gmail.com>
2025-01-13 18:04:59 +0900

    commit

--- webpack-5/src/index.js
+++ webpack-5/src/index.js
@@ -2,16 +2,21 @@ import {
   Cartesian3,
   Ion,
   Viewer,
-  Terrain,
   Math as CesiumMath,
-  createOsmBuildingsAsync,
   ScreenSpaceEventHandler,
   ScreenSpaceEventType,
   defined,
   Color,
+  createGooglePhotorealistic3DTileset,
+  GeoJsonDataSource,
+  Cartographic,
+  VerticalOrigin,
+  HeightReference,
 } from "cesium";
 import "cesium/Build/Cesium/Widgets/widgets.css";
 import "./css/main.css";
+import { ApiKeyManager } from "@esri/arcgis-rest-request";
+import { serviceArea } from "@esri/arcgis-rest-routing";
 
 // Math as CesiumMath
 
@@ -36,8 +41,13 @@ function calculatePlaneDistance(position1, position2) {
   return planeDistance;
 }
 
+// const viewer = new Viewer("cesiumContainer", {
+//   terrain: Terrain.fromWorldTerrain(),
+// });
 const viewer = new Viewer("cesiumContainer", {
-  terrain: Terrain.fromWorldTerrain(),
+  timeline: false,
+  animation: false,
+  globe: false,
 });
 
 // Fly the camera to San Francisco at the given longitude, latitude, and height.
@@ -51,8 +61,122 @@ viewer.camera.flyTo({
 
 // Add Cesium OSM Buildings, a global 3D buildings layer.
 // @ts-ignore
-const buildingTileset = await createOsmBuildingsAsync();
-viewer.scene.primitives.add(buildingTileset);
+// const buildingTileset = await createOsmBuildingsAsync();
+// viewer.scene.primitives.add(buildingTileset);
+
+// Add a global base layer using the Google Maps Platform Map Tiles API
+try {
+  // @ts-ignore
+  const tileset = await createGooglePhotorealistic3DTileset();
+  viewer.scene.primitives.add(tileset);
+} catch (error) {
+  console.log(`Failed to load tileset: ${error}`);
+}
+
+// ArcGIS location services REST API
+const authentication = ApiKeyManager.fromKey(
+  "AAPT3NKHt6i2urmWtqOuugvr9SZ2lQsIKWCKGUFYqC7k4zVVscopv4hDV_cr72qNTv_VHqlkffhYEElHgIGai2Sul5LqciKBoE90HGcnGb-SxuiPHWYrsWwXml7qEBF0pPZzFVmo86RKwQVn62g1BD854USesrBDYH8mjNFcascbodpg17ba4h45eoD9qLJa316Wr1xAT14DjLqm8r2vLzBWOevGbQCBPvlJnarblatDblA.",
+);
+
+async function getServiceArea(cartographic) {
+  const coordinates = [
+    CesiumMath.toDegrees(cartographic.longitude),
+    CesiumMath.toDegrees(cartographic.latitude),
+  ];
+
+  let geojson;
+  try {
+    const response = await serviceArea({
+      facilities: [coordinates],
+      authentication,
+    });
+
+    geojson = response.saPolygons.geoJson;
+  } catch (error) {
+    console.log(`Failed to load service area: ${error}`);
+  }
+
+  if (!defined(geojson)) {
+    return;
+  }
+
+  let dataSource;
+  try {
+    dataSource = await GeoJsonDataSource.load(geojson, {
+      clampToGround: true,
+    });
+    viewer.dataSources.add(dataSource);
+  } catch (error) {
+    console.log(`Failed to load geojson: ${error}`);
+  }
+
+  if (!defined(dataSource)) {
+    return;
+  }
+
+  // Style the results
+  const entities = dataSource.entities.values;
+
+  for (let i = 0; i < entities.length; i++) {
+    const feature = entities[i];
+    feature.polygon.outline = false;
+
+    if (feature.properties.FromBreak == 0) {
+      feature.polygon.material = Color.fromHsl(0.5833, 0.8, 0.9, 0.5);
+    } else if (feature.properties.FromBreak == 5) {
+      feature.polygon.material = Color.fromHsl(0.5833, 0.9, 0.7, 0.5);
+    } else {
+      feature.polygon.material = Color.fromHsl(0.5833, 1.0, 0.4, 0.5);
+    }
+  }
+
+  const scene = viewer.scene;
+  scene.invertClassification = true;
+  scene.invertClassificationColor = new Color(0.4, 0.4, 0.4, 1.0);
+}
+
+// At a location in San Francisco
+viewer.camera.setView({
+  destination: Cartesian3.fromDegrees(-122.38329, 37.74015, 16000),
+  orientation: {
+    pitch: CesiumMath.toRadians(-70.0),
+  },
+});
+const cartesian = Cartesian3.fromDegrees(-122.39429, 37.78988);
+getServiceArea(Cartographic.fromCartesian(cartesian));
+
+const marker = viewer.entities.add({
+  name: "start",
+  billboard: {
+    verticalOrigin: VerticalOrigin.BOTTOM,
+    heightReference: HeightReference.CLAMP_TO_GROUND,
+    image: "./marker.svg",
+    disableDepthTestDistance: Number.POSITIVE_INFINITY,
+    scale: 1,
+  },
+});
+
+marker.position = cartesian;
+
+// Add utility to our app by allowing the user to choose the position used as input for the spatial query.
+viewer.screenSpaceEventHandler.setInputAction((movement) => {
+  viewer.dataSources.removeAll();
+  viewer.scene.invertClassification = false;
+  marker.show = false;
+
+  const pickedPosition = viewer.scene.pickPosition(movement.position);
+
+  if (!defined(pickedPosition)) {
+    return;
+  }
+
+  marker.position = pickedPosition;
+  marker.show = true;
+  viewer.scene.invertClassification = true;
+
+  const cartographic = Cartographic.fromCartesian(pickedPosition);
+  getServiceArea(cartographic);
+}, ScreenSpaceEventType.LEFT_CLICK);
 
 var positions = [];
 var handler = new ScreenSpaceEventHandler(viewer.canvas);
